a26529a8ff4e1ab646352fc7d1cbaca0f6b88dde
tamashionut <tamash_ionut@hotmail.com>
2014-03-09 19:50:59 +0200

    Added initial draft for IBus contract.

---
+++ XFlow/XFlow.Bus/IBus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFlow.Bus
+{
+    public interface IBus
+    {
+        void Send<T>(T message);
+
+        void Send(object message);
+
+        void Subscribe<T>(Action<T> handler);
+
+        void Subscribe(Action<object> handler);
+
+    }
+}
