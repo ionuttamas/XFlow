ef2664de5c5d3bc698d88871b7ace4b3fa268eb9
tamashionut <tamash_ionut@hotmail.com>
2014-03-09 20:55:10 +0200

    Added initial version of messaging structure

--- XFlow/XFlow.Bus/IBus.cs
+++ XFlow/XFlow.Bus/IBus.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace XFlow.Bus
 {
+    /// <summary>
+    /// Defines a contract that will be implemented by any underlying ESB used for messaging.
+    /// </summary>
     public interface IBus
     {
-        void Send<T>(T message);
+        /// <summary>
+        /// Sends the specified IMessage message on the bus.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void Send(IMessage message);
 
+        /// <summary>
+        /// Sends the specified generic message on the bus.
+        /// </summary>
+        /// <param name="message">The message.</param>
         void Send(object message);
 
-        void Subscribe<T>(Action<T> handler);
+        /// <summary>
+        /// Subscribes the specified handler to handle the IMessage message that is received on the bus.
+        /// </summary>
+        /// <param name="handler">The handler.</param>
+        void Subscribe(Action<IMessage> handler);
 
+        /// <summary>
+        /// Subscribes the specified handler to handle the generic message that is received on the bus.
+        /// </summary>
+        /// <param name="handler">The handler.</param>
         void Subscribe(Action<object> handler);
 
     }
